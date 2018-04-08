54795d5791761e97ff3d4b6f3b4f335a52421209
laulaunidas <laulaunidas@gmail.com>
2018-04-08 21:37:36 +0200

    Modification : ajout commentaires  et gestion d'erreur chiffre negatif
    Pour plus de compréhension, j'ai ajouté des commentaires.
    
    J'ai ajouté une condition lors de la saisie du nombre.Si le nombre
    rentré est negatif, je le multiplie  par -1.

--- script.js
+++ script.js
@@ -1,37 +1,46 @@
+/**
+ * Les variables
+ */
+
 let ligne;
 let colonne;
 let index;
-
-const span = document.querySelector("span");
-const table = document.querySelector("table");
 let html = "";
 let nombre;
+const span = document.querySelector("span");
+const table = document.querySelector("table");
 
 
 
+/**
+ * Main
+ */
 
+// tant que l'utilisateur ne rentre pas un chiffre, on passe pas à la suite
 do {
     nombre = parseFloat(window.prompt("Veuillez saisir un nombre :"));
+    if (nombre < 0) nombre *= -1;
 }
-while (isNaN(nombre) == true);
+while (isNaN(nombre));
 
+// Ajout/modification du nombre dans le titre de la pge html Table de multiplication de x
 span.innerHTML = nombre;
 
-
+//construction du inner html du tableau
 for (ligne = 1; ligne <= nombre; ligne++) {
-    html += "<tr>"
-    for (colonne = 1; colonne <= nombre; colonne++) {
-        if (ligne == colonne) {
-            html += `<td class="carre"> ${ligne * colonne} </td>`
-        }
+	html += "<tr>";
+	for (colonne = 1; colonne <= nombre; colonne++) {
+		if (ligne == colonne) {
+			html += `<td class="carre"> ${ligne * colonne} </td>`;
+		}
 
 
-        else {
-            html += `<td> ${ligne * colonne} </td>`;
-        }
-    }
+		else {
+			html += `<td> ${ligne * colonne} </td>`;
+		}
+	}
 
-    html += "</tr>";
+	html += "</tr>";
 }
-
+// On met le contenu du tableau dans le tableau
 table.innerHTML = html;
